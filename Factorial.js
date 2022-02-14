cd8825866ba8c5016427a2c6726715ef1e995c37
EthanKettle <97110734+EthanKettle@users.noreply.github.com>
2022-02-14 11:21:28 -0700

    FIRST

---
+++ Factorial.js
@@ -0,0 +1,15 @@
+function factorial (f,fullFactorial) {
+    let i = 0
+    while (i < f) {
+        fullFactorial = fullFactorial * f
+        f--;
+        i++;
+        return fullFactorial
+        return f
+    }
+}
+
+let factor = 7;
+let num = 1;
+
+console.log(factorial(factor,num))
