81e849125a1acb5c3d7b0e1d474c5d255de77cf3
EthanKettle <97110734+EthanKettle@users.noreply.github.com>
2022-02-19 20:16:58 -0700

    all here

--- Factorial.js
+++ Factorial.js
@@ -1,15 +1,10 @@
-function factorial (f,fullFactorial) {
-    let i = 0
-    while (i < f) {
-        fullFactorial = fullFactorial * f
-        f--;
-        i++;
-        return fullFactorial
-        return f
+function factorial (n) { {
+    if (n < 1){
+        return 1
     }
-}
 
-let factor = 7;
-let num = 1;
+    return n * factorial(n - 1);
+    }
+};
 
-console.log(factorial(factor,num))
+console.log(factorial(7))
